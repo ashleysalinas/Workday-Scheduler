13e7900a47135160d642777099ab761b1c7270a1
ashleysalinas <ashleysalinas98@protonmail.com>
2021-12-29 11:38:19 -0600

    fixed section alignment

--- script.js
+++ script.js
@@ -13,16 +13,17 @@ var hours = ['8 AM', '9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', 
 for (i = 0; i < hours.length; i++) {
     var hourBlock = $('<div>')
     mainArea.append(hourBlock)
-    var hoursSlot = $('<section>');
-    hoursSlot.text(hours[i]);
-    hourBlock.append(hoursSlot)
+    var hoursDisplay = $('<section>');
+    hoursDisplay.text(hours[i]);
+    hoursDisplay.addClass('hourDisplay') //for CSS
+    hourBlock.append(hoursDisplay)
     var textArea = $('<input>')
     hourBlock.append(textArea)
     var saveButton = $('<button>')
     saveButton.text("Save")
     hourBlock.append(saveButton)
     saveButton.on('click', saveHourInfo)
-
+    saveButton.addClass('saveBtn') //CSS
 }
 
 function saveHourInfo() {
