2322006928033a95357a275fe61343dfcf7a21b7
ashleysalinas <ashleysalinas98@protonmail.com>
2021-12-29 11:20:45 -0600

    added date/time display

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 var mainArea = $('.container')
+var dateArea = $('#currentDay')
 
+//Function to display current day using Moment API
+function displayTime() {
+    var rightNow = moment().format('MMM DD, YYYY hh:mm:ss a')
+    dateArea.text("Current Date/Time: " + rightNow)
+}
+setInterval(displayTime, 1);
 var hours = ['8 AM', '9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM'];
 
 
@@ -19,5 +26,5 @@ for (i = 0; i < hours.length; i++) {
 }
 
 function saveHourInfo() {
-    console.log("working")
+   
 }
