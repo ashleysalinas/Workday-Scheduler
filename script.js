321895add0cfd05f7fa60ace17818addb2b4aaeb
ashleysalinas <ashleysalinas98@protonmail.com>
2021-12-29 12:14:21 -0600

    functional save button using localStorage

--- script.js
+++ script.js
@@ -7,9 +7,11 @@ function displayTime() {
     dateArea.text("Current Date/Time: " + rightNow)
 }
 setInterval(displayTime, 1);
+
+
 var hours = ['8 AM', '9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM'];
 
-
+//For loop uses hours array to create buttons/sections in bulk
 for (i = 0; i < hours.length; i++) {
     var hourBlock = $('<div>')
     mainArea.append(hourBlock)
@@ -17,15 +19,25 @@ for (i = 0; i < hours.length; i++) {
     hoursDisplay.text(hours[i]);
     hoursDisplay.addClass('hourDisplay') //for CSS
     hourBlock.append(hoursDisplay)
-    var textArea = $('<input>')
+    var textArea = $('<textarea>')
+    textArea.addClass('textArea') //for CSS
     hourBlock.append(textArea)
     var saveButton = $('<button>')
     saveButton.text("Save")
     hourBlock.append(saveButton)
-    saveButton.on('click', saveHourInfo)
-    saveButton.addClass('saveBtn') //CSS
+    saveButton.addClass('saveBtn') // CSS
 }
 
-function saveHourInfo() {
-   
-}
+
+$('.saveBtn').on('click', function() {
+    let btn = $(this);
+    let id = btn.siblings('section').text() //Uses hour of corresponding hour block as the ID
+    let textInput = btn.siblings('textarea'); //Pulls info from text area
+    let savedText = textInput.val()
+    localStorage.setItem(id, savedText)
+})
+
+
+$('textarea').each(function() {
+    $(this).val(localStorage.getItem($(this).siblings('section').text()))
+})
